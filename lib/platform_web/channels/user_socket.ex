518281b0f589031e4fc4a80387eee204e279aaa5
Robin Böhm <robin.boehm@workshops.de>
2017-10-26 11:55:40 +0200

    feat(socket): add channel room

--- lib/platform_web/channels/user_socket.ex
+++ lib/platform_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule PlatformWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", PlatformWeb.RoomChannel
+  channel "room:*", PlatformWeb.RoomChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
