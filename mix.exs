cb8fc5c191fbd2ac058490691126d53e69ebf25b
Robin Böhm <robin.boehm@workshops.de>
2017-10-26 11:45:30 +0200

    chore(mix): add credo

--- mix.exs
+++ mix.exs
@@ -39,7 +39,8 @@ defmodule Platform.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:mix_test_watch, "~> 0.3", only: :dev}
+      {:mix_test_watch, "~> 0.3", only: :dev},
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
     ]
   end
 
