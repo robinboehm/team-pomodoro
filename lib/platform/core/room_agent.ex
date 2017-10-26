ac4f54a92a35344fa3736db3686f7aea26376ba6
Robin Böhm <robin.boehm@workshops.de>
2017-10-26 14:43:02 +0200

    feat(Core/RoomAgent): Init RoomAgent and test

---
+++ lib/platform/core/room_agent.ex
@@ -0,0 +1,6 @@
+defmodule Platform.Core.RoomAgend do
+  @moduledoc """
+  The RoomAgent to hold state for a single room
+  """
+
+end
---
+++ test/platform/core/room_agent_test.exs
@@ -0,0 +1,4 @@
+defmodule Platform.RoomAgentTest do
+  use ExUnit.Case, async: true
+
+end
