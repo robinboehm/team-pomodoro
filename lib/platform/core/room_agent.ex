5613533fa50157d67e4bf38bad6612184f6d2dcc
Robin Böhm <robin.boehm@workshops.de>
2017-10-26 15:01:45 +0200

    feat(core/room_agent): add basic add_user function

--- lib/platform/core/room_agent.ex
+++ lib/platform/core/room_agent.ex
@@ -2,5 +2,28 @@ defmodule Platform.Core.RoomAgent do
   @moduledoc """
   The RoomAgent to hold state for a single room
   """
+  use Agent
+
+  @doc """
+  Starts a new room.
+  """
+  def start_link(_opts) do
+    Agent.start_link(fn -> %{users: []} end)
+  end
+
+  @doc """
+  Get a list of current users
+  """
+  def list_users(room) do
+    Agent.get(room, &Map.get(&1, :users))
+  end
+
+  @doc """
+  Add a user to the room
+  """
+  def add_user(room, user) do
+    current_userlist = Agent.get(room, &Map.get(&1, :users))
+    Agent.update(room, &Map.put(&1, :users, [%{} | current_userlist]))
+  end
 
 end
--- test/platform/core/room_agent_test.exs
+++ test/platform/core/room_agent_test.exs
@@ -1,4 +1,30 @@
 defmodule Platform.RoomAgentTest do
   use ExUnit.Case, async: true
 
+  alias Platform.Core.RoomAgent
+
+  setup do
+    {:ok, room} = start_supervised(RoomAgent)
+    %{room: room}
+  end
+
+  describe "add_user" do
+    test "adds a user to the current room", %{room: room} do
+      assert RoomAgent.list_users(room) == []
+
+      RoomAgent.add_user(room, %{})
+
+      assert RoomAgent.list_users(room) == [%{}]
+    end
+
+    test "adds more than one user to the current room", %{room: room} do
+      assert RoomAgent.list_users(room) == []
+
+      RoomAgent.add_user(room, %{})
+      RoomAgent.add_user(room, %{})
+
+      assert RoomAgent.list_users(room) == [%{},%{}]
+    end
+  end
+
 end
