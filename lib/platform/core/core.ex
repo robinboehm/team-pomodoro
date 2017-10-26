4f0666fa8174cfdbe50f82e6125196ee1b4286ec
Robin Böhm <robin.boehm@workshops.de>
2017-10-26 11:48:04 +0200

    fix(room): add basic impl and remove unused methods

--- lib/platform/core/core.ex
+++ lib/platform/core/core.ex
@@ -7,24 +7,9 @@ defmodule Platform.Core do
   alias Platform.Core.Room
 
   def list_rooms do
-
+    []
   end
 
-  def get_room!(id), do: nil
+  def get_room!(id), do: %{}
 
-  def create_room(attrs \\ %{}) do
-
-  end
-
-  def update_room(%Room{} = room, attrs) do
-
-  end
-
-  def delete_room(%Room{} = room) do
-
-  end
-
-  def change_room(%Room{} = room) do
-
-  end
 end
