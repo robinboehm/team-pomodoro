95716982217126f5bd0fac290e5a61b1197798fd
Sascha Brink <sascha.brink@gmail.com>
2017-10-26 20:47:32 +0200

    Tidy up room#index

--- lib/platform_web/controllers/room_controller.ex
+++ lib/platform_web/controllers/room_controller.ex
@@ -5,12 +5,13 @@ defmodule PlatformWeb.RoomController do
 
   def index(conn, _params) do
     rooms = Core.list_rooms()
+
     render(conn, "index.html", rooms: rooms)
   end
 
   def show(conn, %{"id" => id}) do
     room = Core.get_room!(id)
+
     render(conn, "show.html", room: room)
   end
-
 end
