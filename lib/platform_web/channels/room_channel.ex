4336a5628a356023b9c740bde7fcbcda876ad905
Robin Böhm <robin.boehm@workshops.de>
2017-10-27 16:14:17 +0200

    chore(credo): make credo happy

--- lib/platform_web/channels/room_channel.ex
+++ lib/platform_web/channels/room_channel.ex
@@ -5,6 +5,10 @@ defmodule PlatformWeb.RoomChannel do
   use PlatformWeb, :channel
 
   alias Platform.Presence
+  alias Platform.Core.TimerRegistry
+  alias Platform.Core.Timer
+
+  @endpoint
 
   def join("room:lobby", _payload, socket) do
     send(self(), :after_join_lobby)
@@ -16,8 +20,8 @@ defmodule PlatformWeb.RoomChannel do
   end
 
   def terminate(msg, socket) do
-    PlatformWeb.Endpoint.broadcast "room:lobby", "room_update:", %{room_id: socket.topic, data: Presence.list(socket)}
-    IO.inspect "User left"
+    payload = %{room_id: socket.topic, data: Presence.list(socket)}
+    @endpoint.broadcast "room:lobby", "room_update:", payload
   end
 
   def handle_info(:after_join_lobby, socket) do
@@ -47,8 +51,8 @@ defmodule PlatformWeb.RoomChannel do
 
   defp receive_current_counter(socket) do
     # Publish current timer-value
-    timer = Platform.Core.TimerRegistry.create_or_get(socket.assigns.room_id)
-    counter = Platform.Core.Timer.info(timer)
+    timer = TimerRegistry.create_or_get(socket.assigns.room_id)
+    counter = Timer.info(timer)
     push socket, "counter", %{value: counter}
 
     socket
@@ -64,22 +68,22 @@ defmodule PlatformWeb.RoomChannel do
   # broadcast to everyone in the current topic (room:lobby).
   def handle_in("start", payload, socket) do
     # broadcast socket, "shout", payload
-    timer = Platform.Core.TimerRegistry.create_or_get(socket.assigns.room_id)
-    Platform.Core.Timer.start(timer)
+    timer = TimerRegistry.create_or_get(socket.assigns.room_id)
+    Timer.start(timer)
     {:noreply, socket}
   end
 
   def handle_in("stop", payload, socket) do
     # broadcast socket, "shout", payload
-    timer = Platform.Core.TimerRegistry.create_or_get(socket.assigns.room_id)
-    Platform.Core.Timer.stop(timer)
+    timer = TimerRegistry.create_or_get(socket.assigns.room_id)
+    Timer.stop(timer)
     {:noreply, socket}
   end
 
   def handle_in("reset", payload, socket) do
     # broadcast socket, "shout", payload
-    timer = Platform.Core.TimerRegistry.create_or_get(socket.assigns.room_id)
-    Platform.Core.Timer.reset(timer)
+    timer = TimerRegistry.create_or_get(socket.assigns.room_id)
+    Timer.reset(timer)
     {:noreply, socket}
   end
 end
