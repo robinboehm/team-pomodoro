a424dbaf0ee441cf418e796a22f6219f49f77719
Robin Böhm <robin.boehm@workshops.de>
2017-10-27 12:25:01 +0200

    feat(timer): add stop methods

--- lib/platform/core/room_ timer.ex
+++ lib/platform/core/room_ timer.ex
@@ -4,6 +4,8 @@ defmodule Platform.Core.RoomTimer do
   """
   use GenServer
 
+  @default_time 25*60 # 25minutes pomodoro
+
   ## Client API
 
   @doc """
@@ -20,6 +22,20 @@ defmodule Platform.Core.RoomTimer do
     GenServer.cast(__MODULE__, {:start})
   end
 
+  @doc """
+
+  """
+  def stop() do
+    GenServer.cast(__MODULE__, {:stop})
+  end
+
+  @doc """
+
+  """
+  def reset() do
+    GenServer.cast(__MODULE__, {:reset})
+  end
+
   ## Server Callbacks
 
   @doc """
@@ -32,8 +48,7 @@ defmodule Platform.Core.RoomTimer do
   """
   def init(:ok) do
     running = false
-    counter  = 25*60 # 25minutes pomodoro
-    IO.inspect Process.send_after(self(), :tick, 1_000)
+    counter = @default_time
     {:ok, {running, counter}}
   end
 
@@ -47,19 +62,21 @@ defmodule Platform.Core.RoomTimer do
     {:noreply, {running, counter}}
   end
 
-  def handle_cast({:stop}, {_, counter}) do
+  def handle_cast({:stop}, {_running, _counter}) do
+    running = false
+    {:noreply, {running, @default_time}}
+  end
+
+  def handle_info(:tick, {true = _running, 0 = counter}) do
     running = false
     {:noreply, {running, counter}}
   end
-
   def handle_info(:tick, {true = running, counter}) do
     Process.send_after(self(), :tick, 1_000)
-    IO.inspect counter
-
     PlatformWeb.Endpoint.broadcast("room:6A0466FA-38DD-45D9-B75B-8476D2F81F07", "counter", %{value: counter})
     {:noreply, {running, (counter-1)}}
   end
   def handle_info(:tick, {false = running, counter}) do
-    {:noreply, {running, (counter)}}
+    {:noreply, {running, counter}}
   end
 end
