777d33125171ebe08557be3bff655ca28165e605
Robin Böhm <robin.boehm@workshops.de>
2017-11-03 10:22:48 +0100

    refactor(core/timer): replace boolean state value with atoms

--- lib/platform/core/timer.ex
+++ lib/platform/core/timer.ex
@@ -55,47 +55,42 @@ defmodule Platform.Core.Timer do
 
   """
   def init(name) do
-    running = false
     counter = @default_time
-    {:ok, {running, counter, name}}
+    {:ok, {:paused, counter, name}}
   end
 
   @doc """
   """
-  def handle_cast(:start, {false = _running, counter, name}) do
+  def handle_cast(:start, {:paused, counter, name}) do
     # Start the timer
     Process.send_after(self(), :tick, 1_000)
 
-    running = true
-    {:noreply, {running, (counter - 1), name}}
+    {:noreply, {:running, (counter - 1), name}}
   end
-  def handle_cast(:start, {running, counter, name}) do
-    {:noreply, {running, counter, name}}
+  def handle_cast(:start, {:running, counter, name}) do
+    {:noreply, {:running, counter, name}}
   end
 
-  def handle_cast(:stop, {_running, counter, name}) do
-    running = false
-    {:noreply, {running, counter, name}}
+  def handle_cast(:stop, {:running, counter, name}) do
+    {:noreply, {:paused, counter, name}}
   end
 
   def handle_cast(:reset, {_running, _counter, name}) do
-    running = false
     counter = @default_time
     @endpoint.broadcast("room:#{name}", "counter", %{value: counter})
-    {:noreply, {running, counter, name}}
+    {:noreply, {:paused, counter, name}}
   end
 
-  def handle_info(:tick, {true = _running, 0 = counter, name}) do
-    running = false
-    {:noreply, {running, counter, name}}
+  def handle_info(:tick, {:running, 0 = counter, name}) do
+    {:noreply, {:paused, counter, name}}
   end
-  def handle_info(:tick, {true = running, counter, name}) do
+  def handle_info(:tick, {:running, counter, name}) do
     Process.send_after(self(), :tick, 1_000)
     @endpoint.broadcast("room:#{name}", "counter", %{value: counter})
-    {:noreply, {running, (counter - 1), name}}
+    {:noreply, {:running, (counter - 1), name}}
   end
-  def handle_info(:tick, {false = running, counter, name}) do
-    {:noreply, {running, counter, name}}
+  def handle_info(:tick, {:paused, counter, name}) do
+    {:noreply, {:paused, counter, name}}
   end
 
   def handle_call(:info, _from, {running, counter, name}) do
